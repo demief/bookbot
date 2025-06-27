856e93b60df2ad894680f4a18adce8e4047221c1
demief <Efe@tostapane-pro.station>
2025-06-27 10:45:56 +0200

    update main.py and stats.py — add report functionality, and usage of sys.argv

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import sys
 from stats import count_words
 from stats import count_characters
+from stats import sort_dictionary
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,8 +13,21 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    text = get_book_text(sys.argv[1])
+
     num_words = count_words(text)
     # print(f"{num_words} words found in the document")
-    print(count_characters(text))
+
+    # print(count_characters(text))
+
+    character_list_sorted = sort_dictionary(count_characters(text), num_words)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in character_list_sorted:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
 main()
--- stats.py
+++ stats.py
@@ -6,11 +6,22 @@ def count_words(text):
     number_of_words = len(get_words(text))
     return number_of_words
 
-characters = {}
 def count_characters(text):
+    characters = {}
     for char in text.lower():
         if char not in characters:
             characters[f"{char}"] = 1
         else:
             characters[f"{char}"] = characters[f"{char}"] + 1
     return characters
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dictionary(dic_characters, count_characters):
+    sorted_dictionary = []
+    for char in dic_characters:
+        if char.isalpha():
+            sorted_dictionary.append({"char": char, "num": dic_characters[char]})
+    sorted_dictionary.sort(reverse = True, key = sort_on)
+    return sorted_dictionary
